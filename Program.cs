965267193b86e26cd4a8d3f0fe2d419d9978a013
Luis <m.luishernandez@outlook.com>
2020-11-03 16:44:08 -0500

    First commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Investment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Investment");
+        }
+    }
+}
